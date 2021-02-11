902960d684109bfb4b074468afe726b875cbce56
felipecarmonar23 <pipetm12@hotmail.com>
2021-02-11 07:40:31 -0500

    clase 2

---
+++ clases/operacionesclase2.py
@@ -0,0 +1,12 @@
+numeroA = 87
+numeroB = 83
+sumar = numeroA + numeroB
+print("el resultado de la suma es", sumar)
+restar = numeroA - numeroB
+print("el resultado de la resta es", restar)
+multiplicar = numeroA * numeroB
+print("el resultado de la multiplicacion es", multiplicar)
+dividir = numeroA / numeroB
+print("el resultado de la division es", dividir)
+exponente = numeroA ** numeroB
+print("el resultado del exponente es", exponente)
