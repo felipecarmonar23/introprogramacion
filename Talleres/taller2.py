b98052b77f3ddbf46595af32b858fee228c20f64
felipecarmonar23 <pipetm12@hotmail.com>
2021-02-17 15:03:17 -0500

    taller #2 terminado

--- Talleres/taller2.py
+++ Talleres/taller2.py
@@ -8,8 +8,22 @@ MENSAJE_DIVISION = "la division de A/B es"
 MENSAJE_EXPONENTE = "el exponente de A**B es"
 
 #-----entrada al codigo-----#
+#aca solo estan permitidos los numeros enteros
+print("#"*15, "NUMERO ENTERO", "#"*15)
 numeroA = int(input(PREGUNTA_numeroA))
 numeroB = int(input(PREGUNTA_numeroB))
+ismayorA = numeroA >= numeroB
+print ("la ecuacion de A >= a B es", ismayorA)
+print (MENSAJE_SUMA, numeroA + numeroB)
+print (MENSAJE_RESTA, numeroA - numeroB)
+print (MENSAJE_MULTIPLICACION, numeroA * numeroB)
+print (MENSAJE_DIVISION, numeroA / numeroB)
+print (MENSAJE_EXPONENTE, numeroA ** numeroB)
+
+#aca solo estan permitidos los numeros decimales
+print("#"*15, "NUMERO DECIMAL", "#"*15)
+numeroA = float(input(PREGUNTA_numeroA))
+numeroB = float(input(PREGUNTA_numeroB))
 ismayorA = numeroA >= numeroB
 print ("la ecuacion de A >= a B es", ismayorA)
 print (MENSAJE_SUMA, numeroA + numeroB)
